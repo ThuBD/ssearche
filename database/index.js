0c4bb0a5067d67407c45e627598885d83a84ee33
ansonao <ansonao10@gmail.com>
2018-07-19 22:05:40 -0700

    Change the schema for users, create the schema for jobs, create a function to initialize a user.

--- database/index.js
+++ database/index.js
@@ -6,22 +6,67 @@ const userSchema = mongoose.Schema({
 	"username": String,
 	"password": String,
 	"user_email": String,
-	"created_at": Date,
+	"created_at": String,
 	"jobs": Array,
-	"graphs": Array // id of the graphs
+	"graphs": Number // id of the graphs
 });
+
+const jobsSchema = mongoose.Schema({
+	"company_name": String,
+	"position": String,
+	"location": String,
+	"expected_salary": Number,
+	"home_address": String,
+	"position_description": String,
+	"resume_or_cv": String,
+	"cover_letter": String,
+	"personal_rating": Number,
+	"display": Boolean
+})
 
 const graphsSchema = mongoose.Schema({
 	"applications": Array,
 	"application_status": Array,
 	"offered_salaries": Array,
 	"stages": Array,
-	"self_reflection": Array,
+	"self_reflection": Array
 });
 
 const Users = mongoose.model('Users', userSchema);
+const jobsSchema = mongoose.model('Jobs', jobsSchema);
 const Graphs = mongoose.model('Graphs', graphsSchema);
 
-// const saveUserInit = (userInput) => {
-	
-// }
+const saveUserInit = (userInput) => {
+	bcrypt.hash(userInput.password, 10, (err, res) => {
+		if (err) {
+			console.log(err);
+			return;
+		} else {
+			var data = {};
+			data.username = userInput.username;
+			data.password = res;
+			data.user_email = userInput.email;
+			var currentTime = new Date();
+			var month = currentTime.getMonth() + 1;
+			var date = currentTime.getDate();
+			var year = currentTime.getFullYear();
+			var hour = currentTime.getHours();
+			var minute = currentTime.getMinutes();
+			var second = currentTime.getSeconds();
+			data.created_at = `${month}/${date}/${year} ${hour}:${minute}:${second}`;
+			data.jobs = [];
+			data.graphs = 0;
+			let newUser = new Users(data);
+			newUser.save((err, user) => {
+				if (err) {
+					console.error(err);
+					return err;
+				} else {
+					return user;
+				}
+			})
+		}
+	})	
+};
+
+module.exports.saveUserInit = saveUserInit;
