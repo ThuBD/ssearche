735e36e3d41d9f314b24f2b0147a3c4f74cab652
alilthumuch <alilthumuch@gmail.com>
2018-07-19 22:18:49 -0700

    set up webpack, app.jsx, input field

---
+++ client/src/select.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+var select = (props) => {
+    return (
+      <div>
+        <input type = "text"></input>
+        <input type = "submit"></input>
+      </div>
+    )
+}
+
+export default select
+         
---
+++ client/src/userinput.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+var userInput = (props) => {
+    return (
+      <div>
+        <input type = "text">Name</input>
+        <input type = "submit">Company</input>
+      </div>
+    )
+}
+
+export default userInput 
