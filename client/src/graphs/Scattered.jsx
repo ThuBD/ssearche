2a6a57a77dec0d91d9240810439961385f468947
Jae <jaebae1111@gmail.com>
2018-07-26 21:48:39 -0700

    added fakerjs and moment to the project

--- client/src/graphs/Scattered.jsx
+++ client/src/graphs/Scattered.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import faker from 'faker'
 
 class Scattered extends Component {
   constructor(props) {
@@ -8,8 +9,28 @@ class Scattered extends Component {
     }
   }
 
-  componentDidUpdate () {
-    
+  componentDidMount () {
+    // For now, create some fake data upon mounting;
+    // Need Company name, date accepted, accepted status, salary offered
+    let randomNumb;
+
+    let numbApplied = 20 + Math.ceil(300 * Math.random());
+    let data = []
+    for (var i = 0; i < numbApplied; i++) {
+      randomNumb =  Math.random();
+      data[i] = {};
+      data[i].dateApp = faker.date.between('2018-09-01', '2019-01-14');
+      data[i].company = faker.company.companyName();
+      if (randomNumb > 0.95) {
+        data[i].status = 'accepted';
+      } else {
+        data[i].status = 'rejected';
+      }
+    }
+    console.log(data[0].dateApp);
+
+    data[0].dateAccepted = faker.date.future(0.1, data[0].dateApp);
+    console.log(data[0].dateAccepted);
   }
 
   // renders entire earnings section with YAxis, DataPoints, XAxis, and Descriptions as subcomponents
