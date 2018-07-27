c79c27401077e54f2d7b7553fc8669bb12b9d94a
Jae <jaebae1111@gmail.com>
2018-07-26 20:53:05 -0700

    added graphSelector component to the main App

---
+++ client/src/graphs/Scattered.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+class Scattered extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    
+    }
+  }
+
+  componentDidUpdate () {
+    
+  }
+
+  // renders entire earnings section with YAxis, DataPoints, XAxis, and Descriptions as subcomponents
+  render () {
+    return (
+      <div>
+      Scattered
+      </div>
+    ) 
+  }
+}
+
+export default Scattered;
