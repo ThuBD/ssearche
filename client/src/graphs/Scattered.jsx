c2a699e15dd50d6659178ad3cbe361b0e70a7493
Jae <jaebae1111@gmail.com>
2018-07-27 17:08:27 -0700

    completed creating graph select option component and successfully created fakadata needed for graphs

--- client/src/graphs/Scattered.jsx
+++ client/src/graphs/Scattered.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import faker from 'faker'
+import faker from 'faker';
+import moment from 'moment';
 
 class Scattered extends Component {
   constructor(props) {
@@ -20,17 +21,24 @@ class Scattered extends Component {
       randomNumb =  Math.random();
       data[i] = {};
       data[i].dateApp = faker.date.between('2018-09-01', '2019-01-14');
+      data[i].dateHeard = faker.date.future(0.1, data[i].dateApp);
+      // Get date difference to give weight to higher chance of higher salary over time
+      let a = moment([2018, 8, 1]);
+      let b = moment([data[i].dateHeard.getFullYear(), data[i].dateHeard.getMonth(), data[i].dateHeard.getDate()])
+      // let b = moment([]);
+      let daysSinceFirstApp = b.diff(a, 'days');
       data[i].company = faker.company.companyName();
       if (randomNumb > 0.95) {
         data[i].status = 'accepted';
+        let lowerLimit = 55000 + daysSinceFirstApp * 10000 / (daysSinceFirstApp + 50);
+        let upperLimit = 105000 + daysSinceFirstApp * 40000 / (daysSinceFirstApp + 50);
+        data[i].salary = faker.commerce.price(lowerLimit, upperLimit, 2, "$");
       } else {
         data[i].status = 'rejected';
+        data[i].salary = null;
       }
     }
-    console.log(data[0].dateApp);
-
-    data[0].dateAccepted = faker.date.future(0.1, data[0].dateApp);
-    console.log(data[0].dateAccepted);
+    console.log(data);
   }
 
   // renders entire earnings section with YAxis, DataPoints, XAxis, and Descriptions as subcomponents
